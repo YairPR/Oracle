7144ebba989f7e209da8b9cdf903e4b5a5e9ed3b
Yair Purisaca <eddiepurisaca@gmail.com>
2019-04-29 14:31:29 -0500

    Update grants_for_type_object.sql

--- Scripts/privilegios/grants_for_type_object.sql
+++ Scripts/privilegios/grants_for_type_object.sql
@@ -1,29 +1,29 @@
--- SELECT
-select 'GRANT SELECT ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&rol' || ';'
+_-- SELECT
+select 'GRANT SELECT ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&user_or_rol' || ';'
 from dba_objects 
 where OWNER in ('USERNAME') 
 AND OBJECT_TYPE IN ('TABLE', 'VIEW');
 
 -- WITH GRANT OPTION
-select 'GRANT SELECT ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&usrole WITH GRANT OPTION;' 
+select 'GRANT SELECT ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&user WITH GRANT OPTION;' 
 from dba_objects 
 where owner in ('username')   
 AND OBJECT_TYPE IN ('TABLE')
 
 -- EXECUTE
-select 'GRANT EXECUTE ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&usrole' || ';'
+select 'GRANT EXECUTE ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&user_or_rol' || ';'
 from dba_objects 
 where owner in ('username')   
 AND OBJECT_TYPE IN ('FUNCTION', 'PACKAGE')
 
 -- DEBUG
-select 'GRANT DEBUG ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&usrole' || ';'
+select 'GRANT DEBUG ON ' || OWNER || '.' || OBJECT_NAME || ' TO ' || '&user_or_rol' || ';'
 from dba_objects 
 where owner in ('username') 
 AND OBJECT_TYPE IN ('PACKAGE')
 
 -- SYNONYMS
-select 'CREATE OR REPLACE SYNONYM ' || '&usuario' || '.' || OBJECT_NAME || ' FOR ' || OWNER || '.' || OBJECT_NAME || ';'
+select 'CREATE OR REPLACE SYNONYM ' || '&user' || '.' || OBJECT_NAME || ' FOR ' || OWNER || '.' || OBJECT_NAME || ';'
 from dba_objects 
 where owner in ('username')  
 --AND OBJECT_TYPE IN ('TABLE','FUNCTION')
