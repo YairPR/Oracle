ff0d9692f0115f260a29196e7fdb2c2e88219c1f
Yair Purisaca <eddiepurisaca@gmail.com>
2019-11-23 00:01:24 -0500

    Create txhash.sql

---
+++ Scripts/session/txhash.sql
@@ -0,0 +1,18 @@
+--- sql text por hash
+
+
+accept addr prompt 'Hash do Comando => ';
+def hash=&&1
+col hv		noprint
+col ln		heading 'Line'                  format        9,999
+col text	heading 'SQL Statement Text'    format          A65
+start osmtitle
+select  t.hash_value hv,
+        t.piece ln,
+        t.sql_text text
+from    v$sqlarea a,
+        v$sqltext t
+where   a.hash_value = t.hash_value
+  and   a.hash_value = '&hash'
+order by 1,2
+/
