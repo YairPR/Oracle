b3b451e536959525dbf25eaf81bec5c14bab5572
Yair Purisaca <eddiepurisaca@gmail.com>
2019-11-22 21:17:24 -0500

    Update uactiv2.sql

--- Scripts/session/uactiv2.sql
+++ Scripts/session/uactiv2.sql
@@ -30,16 +30,14 @@ select s.username,
 from gv$session s,
      gv$sesstat t,
      gv$statname n
-where 
---	s.inst_id = 1 and
-      s.status='ACTIVE' and
+where s.status='ACTIVE' and
       n.inst_id = s.inst_id and
       n.name like '%CPU used by this session%' and
       t.inst_id = n.inst_id and
       t.statistic# = n.statistic# and
       t.sid = s.sid and  
       t.inst_id = s.inst_id and
-      s.username is not null and
+      s.username is not null 
 --s.username <> 'FOGLIGHT_MONITOR'
 group by s.username,
          s.inst_id,
@@ -52,3 +50,13 @@ group by s.username,
          s.sql_id,
         s.event
 order by 10 desc;
+
+RESULT:
+---------
+USERNAME		 SID	SERIAL# LOGON_TIME	    SQL_EXEC_START	OSUSER		   SQL_ID	 EVENT				     CPU_USAGE_SEC	 PERC
+----------------- ---------- ---------- ------------------- ------------------- ------------------ ------------- ----------------------------------- ------------- ----------
+DT_CTE_SAS		 313	  18831 20/11/2019 19:53:03 22/11/2019 21:10:35 oracle		   8f8cuqdsgyvrr flashback buf free by RVWR		  54698.78	22.54
+DS_SASTRA_WEB		2248	   5735 21/11/2019 19:51:05 22/11/2019 19:39:04 oracle		   46wut6dwvckc4 cell single block physical read	  44087.88	18.17
+DS_SASTRA_WEB		 262	  15855 21/11/2019 19:51:04 22/11/2019 21:14:49 oracle		   284xca0xdypyg flashback buf free by RVWR		  37407.41	15.42
+DS_AWSBDSAS		2188	  52695 20/11/2019 22:17:55 21/11/2019 21:55:46 rdsdb		   7gms8987utt88 log file sequential read		  33725.02	 13.9
+
