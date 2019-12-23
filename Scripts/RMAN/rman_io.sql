a16785927ef14b3f1d4a7f5264f253098c01325b
Yair Purisaca <eddiepurisaca@gmail.com>
2019-12-23 13:24:08 -0500

    Update rman_io.sql

--- Scripts/RMAN/rman_io.sql
+++ Scripts/RMAN/rman_io.sql
@@ -8,5 +8,4 @@ column close for 9999999999999999
 select device_type "Device", type , filename
 , to_char(open_time, 'mm/dd/yyyy hh24:mi:ss') open
 ,to_char(close_time,'mm/dd/yyyy hh24:mi:ss') close ,elapsed_time ET, effective_bytes_per_second EPS
- from v$backup_async_io
-where rownum <10;
+ from v$backup_async_io;
