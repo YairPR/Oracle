b636bebee60ac00900156d05513379c7c149ebbd
Yair Purisaca <eddiepurisaca@gmail.com>
2019-12-23 12:28:01 -0500

    Create space_df_use.sql

---
+++ Scripts/space/space_df_use.sql
@@ -0,0 +1,14 @@
+select df.tablespace_name, df.file_name, round(df.bytes/1024/1024) totalSizeMB, nvl(round(usedBytes/1024/1024), 0) usedMB, nvl(round(freeBytes/1024/1024), 0) freeMB,
+    nvl(round(freeBytes/df.bytes * 100), 0) freePerc, df.autoextensible
+from dba_data_files df
+    left join (
+        select file_id, sum(bytes) usedBytes
+        from dba_extents
+        group by file_id
+    ) ext on df.file_id = ext.file_id
+    left join (
+        select file_id, sum(bytes) freeBytes
+        from dba_free_space
+        group by file_id
+    ) free on df.file_id = free.file_id
+order by df.tablespace_name, df.file_name
