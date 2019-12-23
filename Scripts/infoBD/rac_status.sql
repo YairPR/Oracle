3121acc675ccb58f2edbf9518a58cf7f57fc3f3a
Yair Purisaca <eddiepurisaca@gmail.com>
2019-12-23 10:48:54 -0500

    Update rac_status.sql

--- Scripts/infoBD/rac_status.sql
+++ Scripts/infoBD/rac_status.sql
@@ -1,3 +1,5 @@
+-- https://www.databasejournal.com/features/oracle/article.php/3681531/Oracle-RAC-Checking-RAC-status-with-SQLOS-level-statements.htm
+
 select instance_name, host_name, archiver, thread#, status
 from gv$instance
  /
