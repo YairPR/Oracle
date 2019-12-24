6de899b9b61e53fe6c73b5cd49903c81ec7bd28d
Yair Purisaca <eddiepurisaca@gmail.com>
2019-12-24 00:25:14 -0500

    Update rman_io.sql

--- Scripts/RMAN/rman_io.sql
+++ Scripts/RMAN/rman_io.sql
@@ -1,11 +1,17 @@
+-- 1MB=1048576
+-- 1GB = MBx1048576
+
 alter session set nls_date_format='DD-MON-YYYY hh24:mi:ss';
 set line 1000
 column Device format a5
 column type format a10
 column filename format a70
+column status format a15
 column open for 9999999999999999
 column close for 9999999999999999
-select device_type "Device", type , filename, status, bytes/1048576 sizeMB
-, to_char(open_time, 'mm/dd/yyyy hh24:mi:ss') open
-,to_char(close_time,'mm/dd/yyyy hh24:mi:ss') close ,elapsed_time/6000 ET[mts], effective_bytes_per_second EPS
+select device_type "Device", type , filename, status, bytes/1048576000 sizeGB
+, to_char(open_time, 'mm/dd/yyyy hh24:mi:ss') open,
+to_char(close_time,'mm/dd/yyyy hh24:mi:ss') close ,
+elapsed_time/6000 ETmts, 
+effective_bytes_per_second EPS
  from v$backup_async_io;
