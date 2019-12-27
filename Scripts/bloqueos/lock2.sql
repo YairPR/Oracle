554d790407e5dcd429a76c83e509ae0f69ea21b7
Yair Purisaca <eddiepurisaca@gmail.com>
2019-12-27 00:32:53 -0500

    Create lock2.sql

---
+++ Scripts/bloqueos/lock2.sql
@@ -0,0 +1,21 @@
+col WAITING_SESSION for 999999 heading 'SID_Wait'
+col HOLDING_SESSION for 999999 heading 'SID Hold'
+col objeto format a40
+col kill_pid for a16
+col last_query for a30
+col machine for a20
+col last_call for 99999
+col program for a20 heading 'Program/Module'
+col xusername for a32 heading 'DB_User - OS_User'
+col kill_sesion for a40
+select a.HOLDING_SESSION, b.status, (b.username || ' - ' || b.osuser) xusername,
+       a.WAITING_SESSION,
+       '@s '|| b.sql_address || ' '|| b.sql_hash_value last_QUERY,
+       substr(b.machine, 1, 20) machine, substr(nvl(b.program, b.module), 1, 20) program,
+       LAST_CALL_ET/60 last_call, b.status,
+       'alter system kill session ''' || b.sid || ',' || b.serial# || ''';' kill_sesion
+from dba_waiters a, v$session b
+where a.MODE_HELD = 'Exclusive'
+  and a.HOLDING_SESSION = b.sid (+)
+  ---and b.status = 'INACTIVE'
+;
