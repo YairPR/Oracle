8c8fdbc01fc5f358c6cc994ee8328ffa9240ae20
Yair Purisaca <eddiepurisaca@gmail.com>
2020-01-17 00:31:53 -0500

    Create table_size.sql

---
+++ Scripts/size_bd_obj/table_size.sql
@@ -0,0 +1,22 @@
+--MB
+
+select
+owner as "Schema"
+, segment_name as "Object Name"
+, segment_type as "Object Type"
+, round(bytes/1024/1024,2) as "Object Size (Mb)"
+, tablespace_name as "Tablespace"
+from dba_segments
+where segment_name=’<table_name>’;
+
+
+--GB
+
+select
+owner as "Schema"
+, segment_name as "Object Name"
+, segment_type as "Object Type"
+, round(bytes/1024/1024/1024,2) as "Object Size (Mb)"
+, tablespace_name as "Tablespace"
+from dba_segments
+where segment_name=’<table_name>’;
