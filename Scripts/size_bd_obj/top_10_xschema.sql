13ffa9fcd43f76bc246961b65c36f745913b222d
Yair Purisaca <eddiepurisaca@gmail.com>
2020-01-17 00:32:53 -0500

    Create top_10_xschema.sql

---
+++ Scripts/size_bd_obj/top_10_xschema.sql
@@ -0,0 +1,8 @@
+select * from (select
+owner as "Schema"
+, segment_name as "Object Name"
+, segment_type as "Object Type"
+, round(bytes/1024/1024/1024,2) as "Object Size (Mb)"
+, tablespace_name as "Tablespace"
+from dba_segments
+where owner=’<schema>’  and segment_type='TABLE' order by 4 desc)  where rownum < 11;
