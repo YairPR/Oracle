fd3e37a5226907940b3615896206f784ad7f7dd4
Yair Purisaca <eddiepurisaca@gmail.com>
2020-02-06 11:31:08 -0500

    Update clean_trc.sh

--- Scripts/alertas_bash/clean_trc.sh
+++ Scripts/alertas_bash/clean_trc.sh
@@ -1,5 +1,6 @@
 . /home/oracle/.bash_profile
-
+set -x
+me=`basename "$0" .sh`
 # Obtiene el alias de la BD desde la variable DBALIAS_{$ORACLE_SID}
 DBALIAS=`eval echo \$\{DBALIAS_${ORACLE_SID}\}`
 [ ! -n "${DBALIAS}" ] && DBALIAS=${ORACLE_SID}
@@ -9,8 +10,8 @@ export CRON_DATE=`date +%Y.%m.%d`
 export CRON_SHOUR=`date +%H.%M`
 export CRON_HOUR=`date +%H:%M`
 export DIRDUMP=${ORACLE_BASE}/
-export DIRWORK=/home/oracle/scripts
-export DIRLOG=${DIRWORK}/logs/cron_$1_${CRON_DATE}_${CRON_SHOUR}.log
+export DIRWORK=/home/oracle/scripts/crons
+export DIRLOG=${DIRWORK}/logs/cron.$me.${CRON_DATE}.${CRON_SHOUR}.log
 
 # #########################
 # Getting ALERTLOG path:
@@ -29,38 +30,38 @@ export DIRDUMP
 # #######################
 # GET SIZE FILESYSTEM
 # #######################
-
+dirtracesize=`du -sh $DIRDUMP`
+cd $DIRDUMP
 df -Ph | grep -vE '^Filesystem|tmpfs|cdrom|proc|boot' | grep /u01 | awk '{ print $6 " " $5 " " $4}' | while read output;
 do
   usep=$(echo $output | awk '{ print $2}' | cut -d '%' -f 1  )
-## partition=$(echo $output | awk '{ print $1 }' ) 
+## partition=$(echo $output | awk '{ print $1 }' )
 ## freespace=$(echo $output | awk '{ print $3 }' )
 
   if [ $usep -ge 90 ]; then
-	echo $output >>  ${SQLLOG}
-        echo "------------------------------------------------------" > ${SQLLOG}
-	echo >> ${SQLLOG}
-	echo "Fecha-Hora de Inicio" >> ${SQLLOG}
-	echo "--------------------" >> ${SQLLOG}
-	echo $CRON_DATE $CRON_HOUR >> ${SQLLOG}
-	echo >> ${SQLLOG}
-	echo "Antes:" >> ${SQLLOG}
-	dirtracesize=`du -sh $DIRDUMP`
-	echo "Tamaño Direcotrio Trace File:" $dirtracesize >> ${SQLLOG}
-	cantfile=`ls -l *.trc *.trm | wc -l`
-	echo "Cantidad de archivos trc trm: " $cantfile >> ${SQLLOG}
-	echo >> ${SQLLOG}
-	echo "Eliminando archivor trc - trm" >> ${SQLLOG}
-	find $DIRDUMP/*.trm -mtime -1 | awk '{print "rm "$1}' | sh
-	find $DIRDUMP/*.trc -mtime -1 | awk '{print "rm "$1}' | sh
-	cd $DIRDUMP
-	echo "Despues:" >> ${SQLLOG}
-	echo "Cantidad de archivos trc trm: " $cantfile >> ${SQLLOG}
-	ls -ltr *.trc *.trm >> ${SQLLOG}
-	echo >> ${SQLLOG}
-	echo "Fecha-Hora de Fin"  >> ${SQLLOG}
-	echo "--------------------" >> ${SQLLOG}
-	echo $CRON_DATE $CRON_HOUR >> ${SQLLOG}
-	echo "------------------------------------------------------" >> ${SQLLOG}
+        echo $output >>  ${DIRLOG}
+        echo "------------------------------------------------------" > ${DIRLOG}
+        echo >> ${DIRLOG}
+        echo "Fecha-Hora de Inicio" >> ${DIRLOG}
+        echo "--------------------" >> ${DIRLOG}
+        echo $CRON_DATE $CRON_HOUR >> ${DIRLOG}
+        echo >> ${DIRLOG}
+        echo "Antes:" >> ${DIRLOG}
+        echo "Tamaño Direcotrio Trace File:" $dirtracesize >> ${DIRLOG}
+        cantfile=`ls -l *.trc *.trm | wc -l`
+        echo "Cantidad de archivos trc trm: " $cantfile >> ${DIRLOG}
+        echo "# ####################################">> ${DIRLOG}
+        echo "# Eliminando archivos trc - trm..." >> ${DIRLOG}
+        echo "# ####################################">> ${DIRLOG}
+        find $DIRDUMP/*.trm -mtime -1 | awk '{print "rm "$1}' | sh
+        find $DIRDUMP/*.trc -mtime -1 | awk '{print "rm "$1}' | sh
+        echo "Despues:" >> ${DIRLOG}
+        echo "Cantidad de archivos trc trm: " $cantfile >> ${DIRLOG}
+        ls -ltr *.trc *.trm >> ${DIRLOG}
+        echo >> ${DIRLOG}
+        echo "Fecha-Hora de Fin"  >> ${DIRLOG}
+        echo "--------------------" >> ${DIRLOG}
+        echo $CRON_DATE $CRON_HOUR >> ${DIRLOG}
+        echo "------------------------------------------------------" >> ${DIRLOG}
   fi
 done
