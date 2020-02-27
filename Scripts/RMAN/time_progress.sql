077acf360e8224b9efa8e8bdd73fa398747dee59
Yair Purisaca <eddiepurisaca@gmail.com>
2020-02-27 12:41:53 -0500

    Create time_progress.sql

---
+++ Scripts/RMAN/time_progress.sql
@@ -0,0 +1,4 @@
+col OPNAME for a30
+select OPNAME,SOFAR/TOTALWORK*100 PCT, trunc(TIME_REMAINING/60) MIN_RESTANTES,
+trunc(ELAPSED_SECONDS/60) MIN_ATEAGORA
+from v$session_longops where TOTALWORK>0 and OPNAME like '%RMAN%';
