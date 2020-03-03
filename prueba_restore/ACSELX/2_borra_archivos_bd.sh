e363ef08b1c6563d44e7724abe905c3e092d1064
Yair Purisaca <eddiepurisaca@gmail.com>
2020-03-03 15:10:05 -0500

    Create 2_borra_archivos_bd.sh

---
+++ prueba_restore/ACSELX/2_borra_archivos_bd.sh
@@ -0,0 +1,83 @@
+cd /oracle001/oradata/PROD/data/
+rm -f *
+
+cd /oracle001/oradata/PROD/indx/
+rm -f *
+
+cd /oracle001/oradata/PROD/undo/
+rm -f *
+
+cd /oracle002/oradata/PROD/data/
+rm -f *
+
+cd /oracle002/oradata/PROD/indx/
+rm -f *
+
+cd /oracle002/oradata/PROD/undo/
+rm -f *
+
+cd /oracle003/oradata/PROD/data/
+rm -f *
+
+cd /oracle003/oradata/PROD/indx/
+rm -f *
+
+cd /oracle003/oradata/PROD/undo/
+rm -f *
+
+cd /oracle004/oradata/PROD/data/
+rm -f *
+
+cd /oracle004/oradata/PROD/indx/
+rm -f *
+
+cd /oracle004/oradata/PROD/undo/
+rm -f *
+
+cd /oracle005/oradata/PROD/data/
+rm -f *
+
+cd /oracle005/oradata/PROD/indx/
+rm -f *
+
+cd /oracle006/oradata/PROD/data/
+rm -f *
+
+cd /oracle006/oradata/PROD/indx/
+rm -f *
+
+cd /oracle006/oradata/PROD/undo/
+rm -f *
+
+cd /oracle002/oradata/PROD/temp/
+rm -f *
+
+cd /oracle003/oradata/PROD/temp/
+rm -f *
+
+cd /oracle004/oradata/PROD/temp/
+rm -f *
+
+cd /oracle002/oradata/PROD/redo/
+rm -f *
+
+cd /oracle003/oradata/PROD/redo/
+rm -f *
+
+cd /oracle004/oradata/PROD/redo/
+rm -f *
+
+cd /oracle001/oradata/PROD/ctls/
+rm -f *
+
+cd /oracle002/oradata/PROD/ctls/
+rm -f *
+
+cd /oracle003/oradata/PROD/ctls/
+rm -f *
+
+cd /backup
+rm -f *
+
+cd /backup/archive/
+rm -f *
