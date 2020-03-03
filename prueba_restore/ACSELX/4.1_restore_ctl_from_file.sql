69a6367817423951a3d8ea09dbaad3f7df76bdae
Yair Purisaca <eddiepurisaca@gmail.com>
2020-03-03 15:19:07 -0500

    Create 4.1_restore_ctl_from_file.sql

---
+++ prueba_restore/ACSELX/4.1_restore_ctl_from_file.sql
@@ -0,0 +1,7 @@
+run {
+allocate channel c1 type 'SBT_TAPE';
+send 'NSR_ENV=(NSR_SERVER=legato.rimac.com.pe,NSR_CLIENT=minerva.rimac.com.pe)';
+set until time "to_date('2020-01-26 06:00:00', 'yyyy-mm-dd hh24:mi:ss')";
+restore controlfile from 'c-4210056700-20200126-03';
+release channel c1;
+}
