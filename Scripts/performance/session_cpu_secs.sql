f55b78c776fcca9c661f59ed771267f7bf529a73
Yair Purisaca <eddiepurisaca@gmail.com>
2020-03-23 23:44:18 -0500

    Create session_cpu_secs.sql

---
+++ Scripts/performance/session_cpu_secs.sql
@@ -0,0 +1,22 @@
+set linesize 200;
+SELECT s.status,s.sid, s.serial#, p.spid as "OS PID",s.username, s.module, st.value/100 as "CPU sec"
+FROM v$sesstat st, v$statname sn, v$session s, v$process p
+WHERE sn.name = 'CPU used by this session' -- CPU
+AND s.status='ACTIVE'
+AND st.statistic# = sn.statistic#
+AND st.sid = s.sid
+AND s.paddr = p.addr
+AND s.last_call_et < 1800 -- active within last 1/2 hour
+AND s.logon_time > (SYSDATE - 240/1440) -- sessions logged on within 4 hours
+ORDER BY st.value;
+
+set linesize 200;
+SELECT s.sid, s.serial#, p.spid as "OS PID",s.username, s.module, st.value/100 as "CPU sec"
+FROM v$sesstat st, v$statname sn, v$session s, v$process p
+WHERE sn.name = 'CPU used by this session' -- CPU
+AND st.statistic# = sn.statistic#
+AND st.sid = s.sid
+AND s.paddr = p.addr
+AND s.last_call_et < 1800 -- active within last 1/2 hour
+AND s.logon_time > (SYSDATE - 240/1440) -- sessions logged on within 4 hours
+ORDER BY st.value;
