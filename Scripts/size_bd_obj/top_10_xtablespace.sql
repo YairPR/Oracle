02db61181b0e27ca1224de8b9c1ca870c4340efa
Yair Purisaca <eddiepurisaca@gmail.com>
2020-06-10 09:47:22 -0500

    Update top_10_xtablespace.sql

--- Scripts/size_bd_obj/top_10_xtablespace.sql
+++ Scripts/size_bd_obj/top_10_xtablespace.sql
@@ -1,8 +1,10 @@
-select * from (select
-owner as "Schema"
+select * from (select owner as "Schema"
 , segment_name as "Object Name"
 , segment_type as "Object Type"
 , round(bytes/1024/1024/1024,2) as "Object Size (Mb)"
 , tablespace_name as "Tablespace"
 from dba_segments
-where tablespace_name =’<tablespace name>' and segment_name='TABLE' order by 4 desc ) where rownum <11;
+where tablespace_name ='&tbsname' 
+and segment_name='TABLE' order by 4 desc ) 
+where rownum < 11
+/
