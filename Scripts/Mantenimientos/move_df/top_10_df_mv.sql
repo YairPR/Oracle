2361e39436b69eead0c0b7a6441a59dcc08a3a3b
Yair Purisaca <eddiepurisaca@gmail.com>
2020-06-22 16:22:20 -0500

    Create top_10_df_mv.sql

---
+++ Scripts/Mantenimientos/move_df/top_10_df_mv.sql
@@ -0,0 +1,16 @@
+ACCEPT location_fs CHAR PROMPT 'Ingresar ruta para evaluar datafiles >'
+set line 300
+column name format a100
+set pagesize 0
+set feedback off
+SET VERIFY OFF
+spool exe_mv_df.sql
+select 'alter database move datafile ' || namefulldf || ' to ' || '''/u02/app/oracle/oradata/PRIDWH/data/' || namedf ||''';'
+ from (
+select a.name namefulldf, substr(name,instr(name,'/',-1)+1) namedf, a.BYTES/1024/1024/1024 as sizedf
+from v$datafile a
+where a.name like '&location_fs%'
+order by 2 desc )
+where rownum < 21
+/
+spool off
