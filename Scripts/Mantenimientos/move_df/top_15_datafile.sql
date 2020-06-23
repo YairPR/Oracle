e258ad12911b69cf9541f8038839380bc4e25882
Yair Purisaca <eddiepurisaca@gmail.com>
2020-06-23 02:38:59 -0500

    Update top_15_datafile.sql

--- Scripts/Mantenimientos/move_df/top_15_datafile.sql
+++ Scripts/Mantenimientos/move_df/top_15_datafile.sql
@@ -18,3 +18,27 @@ where a.name like '&location_fs%'
 order by 2 desc )
 WHERE rownum < 16
 /
+
+------------------------------------------------
+listdf.sql shell mv_df.sh
+SET LINE 300
+SET PAGESIZE 30
+SET FEEDBACK OFF
+SET ECHO OFF
+SET VERIFY OFF
+SET TRIMSPOOL ON
+COLUMN sizedf FORMAT 999,999,999  HEAD 'Size| Datafile GB'
+COLUMN namedf FORMAT A25 HEAD 'Name file|Datafile'
+spool size_datafile_PRIDWH.log
+
+break on report on name_file_datafile skip 1
+compute sum label "Grand Total: " of sizedf on report
+
+SELECT namedf, sizedf
+FROM (
+select  substr(a.name,instr(a.name,'/',-1)+1) namedf, a.BYTES/1024/1024/1024 as sizedf
+from v$datafile a
+where a.name like '/&1%'
+order by 2 desc )
+WHERE rownum < 16
+/
