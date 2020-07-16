477c8e33bf3ec7216dd23cd7a585a84ed016d8d7
Yair Purisaca <eddiepurisaca@gmail.com>
2020-07-16 16:04:44 -0500

    Update size_tb.sql

--- Scripts/size_bd_obj/size_tb.sql
+++ Scripts/size_bd_obj/size_tb.sql
@@ -7,7 +7,8 @@ owner as "Schema"
 , round(bytes/1024/1024,2) as "Object Size (Mb)"
 , tablespace_name as "Tablespace"
 from dba_segments
-where segment_name=’<table_name>’;
+where segment_name='&table_name';
+
 For oracle table size in GB
 
 select
