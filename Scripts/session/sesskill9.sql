72f79a77a60471b68ef4a713e5d27efeea3199f9
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2020-09-24 19:57:38 -0500

    Update sesskill9.sql

--- Scripts/session/sesskill9.sql
+++ Scripts/session/sesskill9.sql
@@ -1,3 +1,4 @@
+-- per user
 select       
         'kill -9 '||a.spid kill
 from v$session b, v$process a
@@ -9,5 +10,24 @@ where b.paddr = a.addr
 --and a.spid = 21037228
 and b.username <> ' '
 and b.username = upper( '&username')
+--and b.module like ( '%ZSDP0098%')
+/
+
+
+-- active sessions
+
+select       
+        'kill -9 '||a.spid kill
+from v$session b, v$process a
+where b.paddr = a.addr
+--and b.serial# = 41396
+--and sid=804
+--and sid in (   1855 ,
+--and status='INACTIVE'
+--and a.spid = 21037228
+and b.username <> ' '
+and b.status = 'ACTIVE'
+and b.username not in ('DBSNMP', 'SYS')
+--and b.username = upper( '&username')
 --and b.module like ( '%ZSDP0098%')
 /
