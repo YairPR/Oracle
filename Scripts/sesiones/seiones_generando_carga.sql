1c3069c464d324b786e7f7ccbc9e394d2ba40091
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2020-11-06 23:01:56 -0500

    Update and rename Scripts/session/uactiv2.sql to Scripts/sesiones/seiones_generando_carga.sql

--- Scripts/sesiones/seiones_generando_carga.sql
+++ Scripts/sesiones/seiones_generando_carga.sql
@@ -3,6 +3,7 @@
 *@Fecha Creacion:          Nov 2017
 *@Descripcion:             Genera lista de usuarios activos por consumo de cpu load
 *@Versión                  2.0
+@version:                  10g,11g +
 *******************************************************************************************************************/
 set linesize 2000
 COLUMN username FORMAT A22
@@ -50,13 +51,22 @@ group by s.username,
          s.sql_id,
         s.event
 order by 10 desc;
+                                 
+/*
 
 RESULT:
 ---------
 USERNAME		 SID	SERIAL# LOGON_TIME	    SQL_EXEC_START	OSUSER		   SQL_ID	 EVENT				     CPU_USAGE_SEC	 PERC
 ----------------- ---------- ---------- ------------------- ------------------- ------------------ ------------- ----------------------------------- ------------- ----------
-DT_CTE_SAS		 313	  18831 20/11/2019 19:53:03 22/11/2019 21:10:35 oracle		   8f8cuqdsgyvrr flashback buf free by RVWR		  54698.78	22.54
-DS_SASTRA_WEB		2248	   5735 21/11/2019 19:51:05 22/11/2019 19:39:04 oracle		   46wut6dwvckc4 cell single block physical read	  44087.88	18.17
-DS_SASTRA_WEB		 262	  15855 21/11/2019 19:51:04 22/11/2019 21:14:49 oracle		   284xca0xdypyg flashback buf free by RVWR		  37407.41	15.42
-DS_AWSBDSAS		2188	  52695 20/11/2019 22:17:55 21/11/2019 21:55:46 rdsdb		   7gms8987utt88 log file sequential read		  33725.02	 13.9
-
+DT_CTE_SAS		1802	  40910 03/11/2020 12:06:16 06/11/2020 20:52:55 oracle		   2th4bg07qr0cf row cache lock 			 221193.35	87.28
+DS_SASCOM_WEB		 490	  56039 06/11/2020 17:07:27 06/11/2020 23:00:43 root		   132cttn0sn1nk latch free				  19286.51	 7.61
+DS_SASACU_WEB		1283	  43283 04/11/2020 12:04:17 06/11/2020 23:00:43 root		   8vm8v9cdkfzat cell single block physical read	   4475.94	 1.77
+DS_SASACU_WEB		 836	  47280 05/11/2020 13:22:59 06/11/2020 23:00:00 root		   0m5v41tbw6y31 cell single block physical read	   1921.97	  .76
+DT_CTE_SAS		1283	  40785 06/11/2020 22:30:29 06/11/2020 22:30:29 dsadm		   20svuaknnnsn2 cell single block physical read	   1731.64	  .68
+SYS			1891	  52469 06/11/2020 21:59:37 06/11/2020 22:56:32 oracle		   dnbj35xvvbg1y cell single block physical read	   1128.35	  .45
+DS_SASACU_WEB		 425	  64066 06/11/2020 11:59:37 06/11/2020 23:00:33 root		   0r6942mgh62u3 read by other session			    821.98	  .32
+DT_CTE_SAS		1736	  25830 06/11/2020 22:18:36 06/11/2020 22:18:36 dsadm		   6v1m7y8p8zy14 cell single block physical read	    675.67	  .27
+DS_SASACU_WEB		 814	  41659 06/11/2020 11:58:18 06/11/2020 23:00:04 root		   0r6942mgh62u3 cell single block physical read	    637.58	  .25
+DS_SASACU_WEB		1890	  63685 06/11/2020 16:42:18 06/11/2020 23:00:43 root		   av1fj0x53tswp gc current request			    352.13	  .14
+DT_CTE_SAS		1315	  15880 06/11/2020 22:43:35 06/11/2020 22:47:39 oracle		   42dhkv4fd7r6c SQL*Net more data from dblink		    211.38	  .08
+*/
