54b6b3c05aae12d63cf16b70e5d00e1e98bfdbd6
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-01-15 03:31:33 -0500

    Update script_killsession2.sql

--- Scripts/sesiones/script_killsession2.sql
+++ Scripts/sesiones/script_killsession2.sql
@@ -31,3 +31,19 @@ and b.username not in ('DBSNMP', 'SYS')
 --and b.username = upper( '&username')
 --and b.module like ( '%ZSDP0098%')
 /
+
+
+set pagesize 1000
+select       
+        'kill -9 '||a.spid kill
+from v$session b, v$process a
+where b.paddr = a.addr
+--and b.serial# = 41396
+--and sid=804
+--and sid in (   1855 ,
+--and status='INACTIVE'
+--and a.spid = 21037228
+and b.username <> ' '
+and b.username not in ('DBSNMP', 'SYS', 'MANTENIMINETO', 'DS_AWSPROD')
+--and b.module like ( '%ZSDP0098%')
+/
