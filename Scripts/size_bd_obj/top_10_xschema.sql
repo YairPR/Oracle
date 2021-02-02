269ce91a9ee0ee35f68d636d2b3f1ba1bc07f46f
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-02-02 11:36:37 -0500

    Update top_10_xschema.sql

--- Scripts/size_bd_obj/top_10_xschema.sql
+++ Scripts/size_bd_obj/top_10_xschema.sql
@@ -5,4 +5,4 @@ owner as "Schema"
 , round(bytes/1024/1024/1024,2) as "Object Size (Mb)"
 , tablespace_name as "Tablespace"
 from dba_segments
-where owner=’<schema>’  and segment_type='TABLE' order by 4 desc)  where rownum < 11;
+where owner='<schema>'  and segment_type='TABLE' order by 4 desc)  where rownum < 11;
