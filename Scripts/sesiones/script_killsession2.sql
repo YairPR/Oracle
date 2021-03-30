99d528a9fa95aa106ecdfa8bb6b7073169964679
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-03-30 11:35:18 -0400

    Update script_killsession2.sql

--- Scripts/sesiones/script_killsession2.sql
+++ Scripts/sesiones/script_killsession2.sql
@@ -14,6 +14,22 @@ and b.username = upper( '&username')
 /
 
 
+-- per user INACTIVE
+select       
+        'kill -9 '||a.spid kill
+from v$session b, v$process a
+where b.paddr = a.addr
+--and b.serial# = 41396
+--and sid=804
+--and sid in (   1855 ,
+and status='INACTIVE'
+--and a.spid = 21037228
+and b.username <> ' '
+and b.username = upper( '&username')
+--and b.module like ( '%ZSDP0098%')
+/
+
+
 -- active sessions
 
 select       
