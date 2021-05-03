83dce75875f71405c6f8a202b3e658fd9de3639d
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-05-02 23:16:17 -0400

    Update bloqueos_en_espera.sql

--- Scripts/bloqueos/bloqueos_en_espera.sql
+++ Scripts/bloqueos/bloqueos_en_espera.sql
@@ -1,5 +1,5 @@
 select /*+ RULE */
-        'ALERTA IBM!, bloqueos en inst. '||l.inst_id ||', SID,serial ' ||l.sid||','||s.serial#||', modulo '||s.module||decode(substr(s.action,1,3),'FRM',', usuario '||substr(s.action,instr( s.action, ':',1,1) + 1, instr( s.action, ':',1,2) - instr( s.action, ':',1,1) - 1),'')  ||', desde '||s.machine||', logon '||to_char(s.LOGON_TIME,'dd/mm/yyyy hh24:mi:ss')||decode(l.lmode,6,', sesion BLOQUEANTE',', sesion EN ESPERA') as mensaje
+        'ALERTA BLOQUEOS!, bloqueos en inst. '||l.inst_id ||', SID,serial ' ||l.sid||','||s.serial#||', modulo '||s.module||decode(substr(s.action,1,3),'FRM',', usuario '||substr(s.action,instr( s.action, ':',1,1) + 1, instr( s.action, ':',1,2) - instr( s.action, ':',1,1) - 1),'')  ||', desde '||s.machine||', logon '||to_char(s.LOGON_TIME,'dd/mm/yyyy hh24:mi:ss')||decode(l.lmode,6,', sesion BLOQUEANTE',', sesion EN ESPERA') as mensaje
 from   gv$lock l,
        gv$session s
 where    ( l.ID1,l.ID2,l.TYPE)  in (select J.ID1,J.ID2,J.TYPE from gv$lock J where J.request>0) and
