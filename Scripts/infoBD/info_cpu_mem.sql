1dfe58974f37a9468079c4dae40bbae02ca5be4f
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-05-02 23:38:51 -0400

    Update info_cpu_mem.sql

--- Scripts/infoBD/info_cpu_mem.sql
+++ Scripts/infoBD/info_cpu_mem.sql
@@ -5,5 +5,6 @@ set lines 299
 select STAT_NAME,to_char(VALUE) as VALUE  ,COMMENTS from v$osstat where stat_name  IN ('NUM_CPUS','NUM_CPU_CORES','NUM_CPU_SOCKETS')
 union
 select STAT_NAME,VALUE/1024/1024/1024 || ' GB'  ,COMMENTS from v$osstat where stat_name  IN ('PHYSICAL_MEMORY_BYTES')
+/
  
 
