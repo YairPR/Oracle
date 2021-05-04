a0d50bbfdfdb9906030bb0ad7b7ff7b89f20afbd
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-05-04 02:16:18 -0400

    Update tbs4.sql

--- Scripts/space/tbs4.sql
+++ Scripts/space/tbs4.sql
@@ -3,9 +3,11 @@
 -- Author       : Tim Hall
 -- Description  : Displays space usage for each datafile.
 -- Requirements : Access to the DBA views.
--- Call Syntax  : @free_space
+-- Call Syntax  : @free_space ----> Ingresar nombre tablespace
 -- Last Modified: 15-JUL-2000 - Created.
 --                12-OCT-2012 - Amended to include auto-extend and maxsize.
+--                04/05/2021  - EYPR
+-- Lleno mas de 30% muestra "XXX" - Vacio muestra "------"
 -- -----------------------------------------------------------------------------------
 SET PAGESIZE 100
 SET LINESIZE 265
@@ -37,3 +39,18 @@ ORDER BY df.tablespace_name,
 
 PROMPT
 SET PAGESIZE 14
+            
+TABLESPACE_NAME      FILE_NAME                                             SIZE_MB    FREE_MB MAX_SIZE_MB MAX_FREE_MB USED_PCT
+-------------------- -------------------------------------------------- ---------- ---------- ----------- ----------- -----------
+INDX_PROD_SSD_MED    +DG_DATA/prod/datafile/indx_prod_ssd_med.1059.1037        700         30       32767       32097  ----------
+                     176311
+
+INDX_PROD_SSD_MED    +DG_DATA/prod/datafile/indx_prod_ssd_med.1062.1037        700         70       32767       32137  ----------
+                     176319
+
+INDX_PROD_SSD_MED    +DG_DATA/prod/datafile/indx_prod_ssd_med.1246.1067       2048       2045       32767       32764  ----------
+                     215895
+
+INDX_PROD_SSD_MED    +DG_DATA/prod/datafile/indx_prod_ssd_med.1247.1067       2048       2045       32767       32764  ----------
+                     215911
+
