f39d7f2e60ab32459908657186c6102c03f022b0
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-05-04 02:21:48 -0400

    Update tbs7.sql

--- Scripts/space/tbs7.sql
+++ Scripts/space/tbs7.sql
@@ -42,4 +42,18 @@ ORDER BY 6 desc)
 select * from CONSULTA
 order by 1;
 
+TABLESPACE_NAME             GB_ALLOC    GB_FREE    GB_USED       FREE_%       USED_%     MAX_GB
+------------------------- ---------- ---------- ---------- ------------ ------------ ----------
+DATA_BDTEST_CTXSYS                .1        .09        .01        93.38         6.62         32
+DATA_PROD_MV                      .1         .1          0        99.94          .06         32
+DATA_PROD_NOTRANS_BIG        2203.49      64.22    2139.27         2.91        97.09    2207.34
+DATA_PROD_NOTRANS_MED         923.66      45.18     878.49         4.89        95.11     919.67
+DATA_PROD_SSD_BIG             891.94      18.44      873.5         2.07        97.93        894
+DATA_PROD_SSD_MED              51.93      20.79      31.14        40.03        59.97        160
+DATA_PROD_TOOLS                35.29      10.04      25.25        28.46        71.54         96
+DATA_PROD_TRANS_BIG          2403.19      27.03    2376.16         1.12        98.88    2427.48
+DATA_PROD_TRANS_MED           887.88      34.62     853.25         3.90        96.10     887.88
+DATA_PROD_TRANS_PAR01           8.78       2.34       6.43        26.70        73.30      96.68
+DATA_PROD_TRANS_PAR02           3.22       2.34        .88        72.73        27.27         32
+
 
