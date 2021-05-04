5aab961c7f7d45b3cf6a4b274b93429c229613ba
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-05-04 14:31:20 -0400

    Update Metrics_Avg_Users_Waiting.sql

--- Scripts/IBM/Incidentes/Metrics_Avg_Users_Waiting.sql
+++ Scripts/IBM/Incidentes/Metrics_Avg_Users_Waiting.sql
@@ -1,5 +1,5 @@
 - Identificar la sesiones o sesiones que causan mas I/O
 - Capturar el SQL_ID
 - Verificar si el plan a cambiado
-- Aplicar outline de requerir
+- Aplicar outline de requerir revisar
 
