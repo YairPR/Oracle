0ba9935196dfa0b7ccf46e4647f7da00ac7e4b0c
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-05-04 02:00:50 -0400

    Update tbs10.sql

--- Scripts/space/tbs10.sql
+++ Scripts/space/tbs10.sql
@@ -32,8 +32,28 @@ SELECT   my_ddf.tablespace_name,
 GROUP BY my_ddf.tablespace_name
 ORDER BY 6 DESC;
 
-TABLESPACE_NAME 		  TOTSIZE     GROWTH	    DFS    TOTFREE	 PERC
+TABLESPACE_NAME                   TOTSIZE     GROWTH        DFS    TOTFREE       PERC
 ------------------------------ ---------- ---------- ---------- ---------- ----------
-DATA_BDSAS_LOGMNR		    32768      32268	    499      32767	  100
-DATA_BDSAS_MON			    32768      32668	     99      32767	  100
-TBSD_AUDIT			        32768      17408	  15359      32767	  100
+TBS_OGG                             32768      31368       1307      32675        100
+DATA_PROD_MV                        32768      32668        100      32768        100
+USERS                              196608     196553         45     196598        100
+DATA_BDTEST_CTXSYS                  32768      32668         93      32761        100
+TBSI_QUEST                          32768      32368         86      32454         99
+INDX_PROD_TRANS_PAR02               65536      63812       1120      64932         99
+TBSD_QUEST                          32768      32268         97      32365         99
+INDX_PROD_SSD_MED                  163840     156296       6235     162531         99
+INDX_PROD_TRANS_PAR03               65536      63212       1120      64332         98
+DATA_PROD_TRANS_PAR03               65536      52436      12000      64436         98
+XDB                                 65536      63312       1125      64437         98
+DATA_PROD_TRANS_PAR02               32768      29468       2400      31868         97
+TBSD_CRITI_BIG                     196608     188460       2370     190830         97
+TBSI_WEB_BIG                       393216     362516       5760     368276         94
+DATA_PROD_TRANS_PAR01               99004      90016       2400      92416         93
+TBSD_NORMA_TMP                     589824     510678      21080     531758         90
+TBSI_WEB_MED                       131072     113383       3445     116828         89
+TBSD_WEB_SMA                       262144     222276      11535     233811         89
+SYSTEM                              98304      80784       6885      87669         89
+TBSI_WEB_SMA                       196608     165429       6003     171432         87
+TBSD_WEB_BIG                       393216     312061      21120     333181         85
+TBSI_OTHERS_SMA                    163840     132996       5382     138378         84
+
