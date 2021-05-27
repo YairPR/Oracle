cf46f1bc50736c3a219b9bfe8701d7b678a00e60
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-05-27 14:54:49 -0400

    Update plan9i.sql

--- Scripts/performance/plan9i.sql
+++ Scripts/performance/plan9i.sql
@@ -1,7 +1,6 @@
 col objeto for a44
-undefine w_hash
+undefine sql_id
 select operation, options, OBJECT_OWNER || '.' || OBJECT_NAME objeto, substr(filter_predicates, 1, 20) filter_predicates
 from V$SQL_PLAN
-where 
-  HASH_VALUE = &&w_hash
+where SQL_ID = '&sql_id'
 order by child_number, id;
