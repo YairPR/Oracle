28a0f43cb54eae1e7929164db65d6d8a0252f730
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-08-25 10:17:24 -0500

    Update kill_session_masivo_xuser.sql

--- Scripts/AWS/RDS/kill_session_masivo_xuser.sql
+++ Scripts/AWS/RDS/kill_session_masivo_xuser.sql
@@ -1,29 +1,33 @@
 WHENEVER SQLERROR EXIT 2
 SET SERVEROUTPUT ON
 
-
 declare
   type table_varchar  is table of varchar2(50);
   var_table_varchar  table_varchar;
-  V_USER varchar2(50);
+  V_SID varchar2(10);
+  V_SERIAL varchar2(10);
 begin
 
-dbms_output.put_line('DROP USERS');
+dbms_output.put_line('KILL SESSION UTEC');
 dbms_output.put_line('----------------');
 
-  var_table_varchar  := table_varchar('UNITIME','DESARROLLO','UTEC','ACADEMICO', 'COMERCIAL', 'CONFIGURACION', 'GENERAL', 'PROGRAMACION', 'SEGURIDAD', 'AUDITORIA', 'MATRICULA', 'PREMATRICULA', 'TIMETABLE', 'CALENDAR', 'FINANZAS', 'EGRESADO', 'TRAMITE', 'RESERVA', 'WORKFLOW');
+  var_table_varchar  := table_varchar('UTEC');
   for elem in 1 .. var_table_varchar.count loop
     dbms_output.put_line(elem || ': ' || var_table_varchar(elem));
 
     BEGIN
-      select UNIQUE username INTO V_USER from dba_users WHERE username = var_table_varchar(elem);
+      select SID, SERIAL INTO V_SID, V_SERIAL from v$session WHERE username = var_table_varchar(elem);
     EXCEPTION
     WHEN NO_DATA_FOUND THEN
       V_USER := NULL;
     END;
 
     IF V_USER IS NOT NULL THEN
-      execute immediate 'DROP USER ' || var_table_varchar(elem) || ' CASCADE';
+      begin
+        rdsadmin.rdsadmin_util.kill(
+        sid    => &sid, 
+        serial => &serial_number);
+       end;
     END IF;
 
   end loop;
