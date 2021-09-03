4244e1b2f5922146f2f796ba6f9d1f49bac24e51
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-09-03 18:33:38 -0500

    Update matar_sesion.sql

--- Scripts/AWS/matar_sesion.sql
+++ Scripts/AWS/matar_sesion.sql
@@ -5,6 +5,20 @@ begin
 end;
 /
 
+dbms_output.put_line('LOCK USER UTEC');
+dbms_output.put_line('----------------'); 
+alter user utec account lock;
+
+dbms_output.put_line('KILL SESSION UTEC');
+dbms_output.put_line('----------------'); 
+BEGIN
+  for session_to_drop in (select SID, SERIAL#  from v$session WHERE username = 'UTEC') 
+  loop
+    rdsadmin.rdsadmin_util.kill(session_to_drop.sid, session_to_drop.serial#, method => 'IMMEDIATE');
+  end loop;
+end;
+/
+
 set pagesize 0
 set line 1000
 select 'exec begin  rdsadmin.rdsadmin_util.kill(sid => ' || SID || ',' || 'serial => ' || SERIAL# || ',method => ' || '''IMMEDIATE''' || '); end;' from v$session WHERE username = 'UTEC';
