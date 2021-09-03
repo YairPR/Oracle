05bbb87195776abe9df585aaa771e10da9d1e2d5
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-09-03 08:38:02 -0500

    Update matar_sesion.sql

--- Scripts/AWS/RDS/matar_sesion.sql
+++ Scripts/AWS/RDS/matar_sesion.sql
@@ -4,3 +4,6 @@ begin
         serial => serial_number);
 end;
 /
+
+select 'exec begin  rdsadmin.rdsadmin_util.kill(sid => ' || SID || ',' || 'serial => ' || SERIAL# || ',method => ' || '''IMMEDIATE''' || '); end;' from v$session WHERE username = 'UTEC';
+
