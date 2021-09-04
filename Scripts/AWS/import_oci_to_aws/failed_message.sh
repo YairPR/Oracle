1d8240e609d19d7e6e2fc0dfe08d8f7c4b573403
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-09-03 19:06:53 -0500

    Create failed_message.sh

---
+++ Scripts/AWS/import_oci_to_aws/failed_message.sh
@@ -0,0 +1,11 @@
+        echo "> F            _____       "
+        echo "> A         .-'     '-.    "
+        echo "> I       .'           '.  "
+        echo "> L      /   .      .    \ "
+        echo "> E     :                 :"
+        echo "> D     |                 |"
+        echo ">       :      ____       :"
+        echo ">        \   /      \    / "
+        echo ">         '.           .'  "
+        echo ">           '-._____.-'    "
+        echo ">                          "[
