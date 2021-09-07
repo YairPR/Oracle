8bd28a379b28bcdc8edcee902961d3e9d7d3eca4
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-09-07 16:11:33 -0500

    Update pctfree.sql

--- Scripts/info_objetos/pctfree.sql
+++ Scripts/info_objetos/pctfree.sql
@@ -6,3 +6,4 @@ select owner, table_name, tablespace_name, status, pct_free, num_rows, last_anal
 union all
 select owner, index_name, tablespace_name, status, pct_free, num_rows, last_analyzed from dba_indexes where index_name IN  ('PK_NCTAIND_COPIA2', 'IDX_NCTAIND_01_COPIA2', 'IDX_NCTAIND_02_COPIA2', 'IDX_NCTAIND_03_COPIA2', 'PK_NCTAIND_COPIA3', 'IDX_NCTAIND_01_COPIA3', 'IDX_NCTAIND_02_COPIA3', 'IDX_NCTAIND_03_COPIA3', 'PK_ACREDITA2_COPIA1', 'PK_ACREDITA2_COPIA2');
 
+ select owner, object_name, LAST_DDL_TIME from dba_objects where object_name in  ('
