651043f5b2513b8775c42273573a1e57265903d2
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-10-19 09:47:16 -0500

    Update 5.2_restore_df.sql

--- prueba_restore/ACSELX/5.2_restore_df.sql
+++ prueba_restore/ACSELX/5.2_restore_df.sql
@@ -5,7 +5,7 @@ allocate channel c3 type 'SBT_TAPE';
 allocate channel c4 type 'SBT_TAPE';
 allocate channel c5 type 'SBT_TAPE';
 allocate channel c6 type 'SBT_TAPE';
-send 'NSR_ENV=(NSR_SERVER=legato.rimac.com.pe,NSR_CLIENT=minerva.rimac.com.pe)';
+send 'NSR_ENV=(NSR_SERVER=legato,NSR_CLIENT= xxx.)';
 set until time "to_date('2020-01-25 22:00:00', 'yyyy-mm-dd hh24:mi:ss')";
 restore datafile '/oracle005/oradata/PROD/data/data_prod_ssd_big_001.dbf', '/oracle001/oradata/PROD/data/data_prod_notrans_big_006.dbf', '/oracle003/oradata/PROD/indx/indx_prod_notrans_med_015.dbf', '/oracle004/oradata/PROD/indx/indx_prod_notrans_med_017.dbf'; 
 release channel c1;
