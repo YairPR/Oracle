b89f810414d1fcfdeaf8a8b956f742ccf2e16b15
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-10-19 09:47:54 -0500

    Update 5_restore_bd.sql

--- prueba_restore/ACSELX/5_restore_bd.sql
+++ prueba_restore/ACSELX/5_restore_bd.sql
@@ -6,7 +6,7 @@ allocate channel c3 type 'SBT_TAPE';
 allocate channel c4 type 'SBT_TAPE';
 allocate channel c5 type 'SBT_TAPE';
 allocate channel c6 type 'SBT_TAPE';
-send 'NSR_ENV=(NSR_SERVER=legato.rimac.com.pe,NSR_CLIENT=minerva.rimac.com.pe)';
+send 'NSR_ENV=(NSR_SERVER=legato,NSR_CLIENT=xxxx)';
 set until time "to_date('2020-01-26 06:00:00', 'yyyy-mm-dd hh24:mi:ss')";
 restore database;
 recover database;
