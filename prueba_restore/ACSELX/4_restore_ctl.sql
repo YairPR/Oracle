bf3961ac8e47d270b5418f73bb2c17a1a9a05acc
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2021-10-19 09:43:37 -0500

    Update 4_restore_ctl.sql

--- prueba_restore/ACSELX/4_restore_ctl.sql
+++ prueba_restore/ACSELX/4_restore_ctl.sql
@@ -1,7 +1,7 @@
 run
 {
 allocate channel c1 type 'SBT_TAPE';
-send 'NSR_ENV=(NSR_SERVER=legato.rimac.com.pe,NSR_CLIENT=minerva.rimac.com.pe)';
+send 'NSR_ENV=(NSR_SERVER=legato,NSR_CLIENT= xx.com.pe)';
 restore controlfile;
 alter database mount;
 }
