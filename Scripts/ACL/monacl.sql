79388a258bb9617e5da9ac2d3adbf2ff7a6d6efb
Yair Purisaca Rivera <eddiepurisaca@gmail.com>
2024-09-10 10:38:39 +0200

    Update monacl.sql
    se ordena el monitoreo y creacion

--- Scripts/ACL/monacl.sql
+++ Scripts/ACL/monacl.sql
@@ -1,13 +1,26 @@
+Nota:
+SMTP: https://oracle-base.com/articles/misc/email-from-oracle-plsql
+utl_mail:  https://dev.dbaclass.com/article/how-to-send-mail-using-utl_mail-in-oracle-11g/
+       
+--validar ACL
+
+3.1.4 Validación de ACL
+Además de las vistas de ACL, los privilegios se pueden verificar utilizando las funciones CHECK PRIVILEGE y CHECK_PRIVILEGE_ACLID del paquete DBMS_NETWORK_ACL_ADMIN.
+
+SELECT DECODE(DBMS_NETWORK_ACL_ADMIN.CHECK_PRIVILEGE(
+'/sys/acls/utl_mail_monitor.xml', 'DBA_MONITOR', 'resolve'),
+1, 'GRANTED', 0, 'DENIED', NULL) PRIVILEGE
+FROM DUAL;
+
+
 SELECT acl,
        principal,
        privilege,
        is_grant,
        TO_CHAR(start_date, 'DD-MON-YYYY') AS start_date,
        TO_CHAR(end_date, 'DD-MON-YYYY') AS end_date
-FROM   dba_network_acl_privileges
+FROM   dba_network_acl_privileges;
 where acl like '%acl_sas_vimed.xml%';
-
-DBA_NETWORK_ACLS
 
 SELECT HOST
       ,lower_port
@@ -16,7 +29,7 @@ SELECT HOST
   FROM dba_network_acls;
 
 
--- crear
+--crear
 BEGIN
 DBMS_NETWORK_ACL_ADMIN.CREATE_ACL (
 acl => '/sys/acls/acl_sas_vimed.xml',
@@ -28,12 +41,11 @@ commit;
 END;
 /
 
-
---permisos
+--Asignar privilegios
 BEGIN
 DBMS_NETWORK_ACL_ADMIN.add_privilege (
-acl => '/sys/acls/acl_sas_vimed.xml',
-principal => 'APP_IAA_INTERFAZ',
+acl => '/sys/acls/smtpapp.aragon.es.xml',
+principal => 'EYPURISACA',
 is_grant => TRUE,
 privilege => 'resolve',
 position => NULL,
@@ -41,18 +53,18 @@ start_date => NULL,
 end_date => NULL); COMMIT;
 END;
 /
---- asignar
+
 BEGIN
-DBMS_NETWORK_ACL_ADMIN.ASSIGN_ACL (
-acl => '/sys/acls/acl_sas_vimed.xml',
-host => 'http://api-test.rimac.com.pe:8043/Services/IntegracionSASWS.asmx',
-lower_port => 8043 ,
-upper_port => 8043 
-);
-COMMIT;
+DBMS_NETWORK_ACL_ADMIN.add_privilege (
+acl => '/sys/acls/smtpapp.aragon.es.xml',
+principal => 'EYPURISACA',
+is_grant => TRUE,
+privilege => 'connect',
+position => NULL,
+start_date => NULL,
+end_date => NULL); COMMIT;
 END;
 /
-
 
 3.1.3 Asignando ACL
 En este caso usaré el puerto 587 aunque también pueden validar con el puerto 25 ó 467.
@@ -70,23 +82,6 @@ COMMIT;
 END;
 /
 
-3.1.4 Validación de ACL
-Además de las vistas de ACL, los privilegios se pueden verificar utilizando las funciones CHECK PRIVILEGE y CHECK_PRIVILEGE_ACLID del paquete DBMS_NETWORK_ACL_ADMIN.
-
-SELECT DECODE(DBMS_NETWORK_ACL_ADMIN.CHECK_PRIVILEGE(
-'/sys/acls/utl_mail_monitor.xml', 'DBA_MONITOR', 'resolve'),
-1, 'GRANTED', 0, 'DENIED', NULL) PRIVILEGE
-FROM DUAL;
-
-SELECT * FROM dba_network_acls;
-
-SELECT acl,
-principal,
-privilege,
-is_grant,
-TO_CHAR(start_date, 'DD-MON-YYYY') AS start_date,
-TO_CHAR(end_date, 'DD-MON-YYYY') AS end_date
-FROM dba_network_acl_privileges;
 
 3.1.5 Eliminando ACL
 Para eliminar un ACL se usa el siguiente comando:
@@ -117,7 +112,9 @@ COMMIT;
 END;
 /
 
-==================================================================
+-------------------
+-------------------
+--------------------
 
 -- Ejecutar como sysdba
 DECLARE
@@ -138,3 +135,39 @@ BEGIN
  
   COMMIT;
 END;
+
+------------------------------
+TEST
+-----------------
+SMTP:
+------------------
+
+--
+CREATE OR REPLACE PROCEDURE send_mail (p_to        IN VARCHAR2,
+                                       p_from      IN VARCHAR2,
+                                       p_message   IN VARCHAR2,
+                                       p_smtp_host IN VARCHAR2,
+                                       p_smtp_port IN NUMBER DEFAULT 25)
+AS
+  l_mail_conn   UTL_SMTP.connection;
+BEGIN
+  l_mail_conn := UTL_SMTP.open_connection(p_smtp_host, p_smtp_port);
+  UTL_SMTP.helo(l_mail_conn, p_smtp_host);
+  UTL_SMTP.mail(l_mail_conn, p_from);
+  UTL_SMTP.rcpt(l_mail_conn, p_to);
+  UTL_SMTP.data(l_mail_conn, p_message || UTL_TCP.crlf || UTL_TCP.crlf);
+  UTL_SMTP.quit(l_mail_conn);
+END;
+/
+---
+
+---
+ENVIAR MAIL
+
+BEGIN
+  send_mail(p_to        => 'eypurisaca@ext.aragon.es',
+            p_from      => 'eypurisaca@ext.aragon.es',
+            p_message   => 'This is a test message.',
+            p_smtp_host => 'smtpapp.aragon.es');
+END;
+/
